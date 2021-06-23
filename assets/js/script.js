b30eb653af6803ef904cf8a4be23698416ec8e81
Guille <galessandri@gmail.com>
2021-06-23 16:51:02 -0400

    navbar-lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 
+    var nav = document.querySelector('nav');
+
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 100) {
+            nav.classList.add('bg-dark', 'shadow');
+        } else {
+            nav.classList.remove('bg-dark', 'shadow');
+        }
+    });
+
 });
