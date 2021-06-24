2ba634c2d4b1b2bc00d53660c224661a41a5fe8c
Guille <galessandri@gmail.com>
2021-06-24 19:01:48 -0400

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,26 @@ $(document).ready(function(){
     var nav = document.querySelector('nav');
 
     window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 100) {
+        if (window.pageYOffset > 200) {
             nav.classList.add('bg-dark', 'shadow');
         } else {
             nav.classList.remove('bg-dark', 'shadow');
         }
     });
 
+    $("#navbarNav").click(function () {
+        $("#navbarNav").toggleClass("navcolor");
+    });
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="collapse"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    const navLinks = document.querySelectorAll('.nav-item')
+    const menuToggle = document.getElementById('navbarNav')
+    const bsCollapse = new bootstrap.Collapse(menuToggle)
+    navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
 });
